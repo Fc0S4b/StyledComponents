9278f5a21a01dcb4a54cea9919259b024c2f9a2e
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 16:21:14 -0400

    separate files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 // npm install styled-components
 import styled from 'styled-components';
-
-// tagged template literal
-const BasicTitle = styled.h1`
-  //uppercase, import.htmlElement
-  text-transform: capitalize;
-  text-align: center;
-`;
+import BasicTitle from './components/BasicTitle';
+import { DefaultButton } from './components/Buttons';
 // en html se escriben nombres únicos de clases generados por styled-components
 
-const DefaultButton = styled.button`
-  background: #645cff;
-  color: #fff;
-  border: none;
-  border-radius: 0.25rem;
-  cursor: pointer;
-  text-transform: capitalize;
-  padding: 0.25rem;
-  display: block;
-  width: 200px;
-  margin: 1rem;
-`;
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
---
+++ src/components/BasicTitle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// tagged template literal
+const BasicTitle = styled.h1`
+  //uppercase, import.htmlElement
+  text-transform: capitalize;
+  text-align: center;
+`;
+
+export default BasicTitle;
