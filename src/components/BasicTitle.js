fe196c94cec0aeeeb960ba9c5755eac68f8f8806
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 16:35:29 -0400

    other props optons

--- src/components/BasicTitle.js
+++ src/components/BasicTitle.js
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 const BasicTitle = styled.h1`
   text-transform: capitalize;
   text-align: center;
-  color: ${(props) =>
+  /* color: ${(props) =>
     props.special &&
-    'red'}; //usando prop con lógica para aplicarla al componente con special
+    'red'}; //usando prop con lógica para aplicarla al componente con special */
+  /* color: ${({ special }) => special && 'red'}; //otra forma */
+  color: ${({ special }) => (special ? 'red' : 'blue')}; //con operador ternario
 `;
 
 export default BasicTitle;
