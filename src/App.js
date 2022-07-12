f2efc0046a08959c75c8f31f255a934af0ac7a8f
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 16:07:51 -0400

    setup in detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+// npm install styled-components
 import styled from 'styled-components';
 
+// tagged template literal
 const BasicTitle = styled.h1`
+  //uppercase, import.htmlElement
   text-transform: capitalize;
   text-align: center;
 `;
-
+// en html se escriben nombres únicos de clases generados por styled-components
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
