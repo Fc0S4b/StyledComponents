eb91d3705e0964caef8841e72b38c00544f246a8
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 17:36:24 -0400

    more complex component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 // npm install styled-components
 import styled from 'styled-components';
-
-import Button from '@mui/material/Button';
-
-// extend react component or with import { styled } from '@mui/material/styles' and the old syntax;
-const StyledBtn = styled(Button)`
-  && {
-    text-transform: capitalize;
-  }
-`;
+import ComplexTitle from './components/ComplexTitle';
+import Random from './components/Random';
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <Button color="primary" variant="contained">
-        Hello World
-      </Button>
-      <StyledBtn color="primary" variant="contained">
-        Hello World
-      </StyledBtn>
+      <ComplexTitle title="more complex title" />
+      <Random />
     </div>
   );
 }
