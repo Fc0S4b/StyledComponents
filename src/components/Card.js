d787655af02b9e47af16bcae563d96a5043e97af
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 18:51:04 -0400

    Card setup

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = () => {
+  return (
+    <StyledCard>
+      <img
+        src="https://dl.airtable.com/.attachmentThumbnails/0446e84c5bca9643de3452a61b2d6195/1b32f48b?ts=1657665615&userId=usrQMwWEPx18KgLcP&cs=30f130f5a5c5dba6"
+        alt="product"
+      />
+      <footer>
+        <h4>product name</h4>
+        <p>$15</p>
+      </footer>
+    </StyledCard>
+  );
+};
+
+const StyledCard = styled.article``;
+
+export default Card;
