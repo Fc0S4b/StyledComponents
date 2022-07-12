dcd1c815ba7c9d61aa1eed3859ddef51d99b90f1
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 18:57:52 -0400

    card style container

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,6 +16,16 @@ const Card = () => {
   );
 };
 
-const StyledCard = styled.article``;
+const StyledCard = styled.article`
+  width: 90vw;
+  max-width: 300px;
+  background: var(--white);
+  border-radius: 0.25rem;
+  img {
+    width: 100%;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+  }
+`;
 
 export default Card;
