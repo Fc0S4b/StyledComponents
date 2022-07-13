b3e96773321d393e52392aa650e786949212bffa
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-13 00:15:39 -0400

    attrs button example

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styled, { css } from 'styled-components/macro';
+// attrs es para manejar los atributos de los componentes, evitar repeticiones o hacer configuraciones generales
+// hacer con objetos hace que todos los botones por default sean de tipo button, para dejar que el btn del form sea submit, se debe hacer con funciones, esta reaccionará de acuerdo al type='submit' que pasa como props al componente
+// const Button = styled.button.attrs({
+//   type: 'button',
+// })...
+const Button = styled.button.attrs((props) => {
+  return { type: props.type || 'button' };
+})`
+  background: var(--primary);
+  border: none;
+  color: white;
+  padding: 0.25rem;
+  cursor: pointer;
+`;
 const Form = () => {
   return (
     <div>
       <h2>some random stuff</h2>
-      <button>click me</button>
+      <Button>click me</Button>
       <form
         css={`
           width: 300px;
@@ -15,7 +29,7 @@ const Form = () => {
       >
         <h2>Form</h2>
         <input type="text" />
-        <button>submit here</button>
+        <Button type="submit">submit here</Button>
       </form>
     </div>
   );
