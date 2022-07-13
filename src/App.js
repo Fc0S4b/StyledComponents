018fe56efe2bcd95094280997a6abe04fb53cb1d
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 23:14:46 -0400

    AS prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // npm install styled-components
 import styled from 'styled-components';
-import Loading from './components/Loading';
-
+import { HipsterButton } from './components/Buttons';
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <Loading />
+      <HipsterButton>click me</HipsterButton>
+      {/* no funcionaría href porque href no es atributo de un elemento button como styled component, la solución es usar as igual al elemento html que deseas que sea*/}
+      <HipsterButton as="a" href="https://www.google.com">
+        click me
+      </HipsterButton>
+      <HipsterButton>click me</HipsterButton>
     </div>
   );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -15,8 +15,15 @@ export const DefaultButton = styled.button`
 
 // extender clases de DefaultButton a nuevo componente HipsterButton que tiene otras propiedades adicionales
 export const HipsterButton = styled(DefaultButton)`
-  width: 400px;
+  width: 150px;
   background: transparent;
   color: #645cff;
   border: 1px solid #645cff;
+  padding: 1rem;
+  display: inline-block;
+  margin-right: 1rem;
+  /* para el prop as = 'a' se debe configurar adicionalmente css para el elemento a que tiene por default otros estilos */
+  text-decoration: none;
+  text-align: center;
+  font-size: 1rem;
 `;
