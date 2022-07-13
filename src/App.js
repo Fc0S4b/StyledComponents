ac17b65c29f751e21325e076f467524f2b6114e0
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-13 00:42:07 -0400

    more complex props - List Example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import Form from './components/Form';
+import List from './components/List';
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <Form />
+      <List />
     </div>
   );
 }
