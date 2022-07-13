371c0cb161cf45cb4222a54065e242524710970e
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-13 00:04:41 -0400

    Form setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import styled from 'styled-components/macro';
-import { DefaultButton } from './components/Buttons';
+import styled from 'styled-components';
+import Form from './components/Form';
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <DefaultButton>click me</DefaultButton>
-      <DefaultButton large>click me</DefaultButton>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled, { css } from 'styled-components/macro';
+const Form = () => {
+  return (
+    <div>
+      <h2>some random stuff</h2>
+      <button>click me</button>
+      <form
+        css={`
+          width: 300px;
+          background: #fff;
+          padding: 2rem;
+          margin-top: 1rem;
+        `}
+      >
+        <h2>Form</h2>
+        <input type="text" />
+        <button>submit here</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
