8110a4e12890056093d66de5104d048086f25c7f
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-12 23:28:42 -0400

    macro and css prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-// npm install styled-components
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import { HipsterButton } from './components/Buttons';
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <HipsterButton>click me</HipsterButton>
-      {/* no funcionaría href porque href no es atributo de un elemento button como styled component, la solución es usar as igual al elemento html que deseas que sea*/}
-      <HipsterButton as="a" href="https://www.google.com">
+      <HipsterButton
+        css={`
+          color: green;
+        `}
+        as="a"
+        href="https://www.google.com"
+      >
         click me
       </HipsterButton>
       <HipsterButton>click me</HipsterButton>
+      {/* css como props por sí solo no funcionaría, se debe añadir al import de styled-components/macro */}
+      {/* agregar /macro a las importaciones de styled-components creará nombre de clases mas descriptivas */}
+      <div
+        css={`
+          color: green;
+        `}
+      >
+        <h2>hello world</h2>
+      </div>
     </div>
   );
 }
